5bab1da69c7793092269904e4ed8cdd3b628bcd1
ishak selvitopu <ishakselvitopu@outlook.com.tr>
2022-03-01 23:43:11 +0300

    1

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using BussinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    { 
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
