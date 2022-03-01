b3110389ac55a8b49e8298138b0f9d9ca697f171
ishak <ishak@ISHAKSELVITOPU>
2022-03-01 23:03:45 +0300

    Add project files.

---
+++ DataAcessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAcessLayer.Abstract;
+using DataAcessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        public void AddCategory(Category category)
+        {
+            context.Add(category);
+            context.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            context.Remove(category);
+            context.SaveChanges();
+        }
+
+        public Category GetByCategoryID(int CategoryID)
+        {
+            return context.Categories.Find(CategoryID);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return context.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            context.Update(category);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataAcessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAcessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.Repositories
+{
+    public class CommentRepository : IGenericDal<Comment>
+    {
+        public void Delete(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment GetByID(int commentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
